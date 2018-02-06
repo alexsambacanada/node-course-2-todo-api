2a929352625ee955355f3051055b56c35a07df66
alexwbai <30630653+alexwbai@users.noreply.github.com>
2018-02-05 22:27:47 -0500

    Add POST /todos route and refactor mongoose

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -21,9 +21,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
     // fineOneAndDelete
 
-    db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-        console.log(result);
-    });
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
 
     //db.close();
 });
