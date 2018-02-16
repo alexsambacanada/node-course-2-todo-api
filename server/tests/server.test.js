8d204712314379d01beb4dcf290c2ed2494e944e
alexwbai <30630653+alexwbai@users.noreply.github.com>
2018-02-16 14:44:39 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -286,9 +286,7 @@ describe('PATCH /todos/:id', () => {
         it('should reject invalid login', (done) => {
             var badpassword = '1';
             // pass in an invalid password
-            // 200 should be 400
-            // xauth token should not exist
-            // tokens array should have a length of zero
+
             request(app)
                 .post('/users/login')
                 .send({
@@ -310,6 +308,26 @@ describe('PATCH /todos/:id', () => {
                     }).catch((e) => done(e));
                 });
 
+        });
+    });
+
+    describe('DELETE /users/me/token', () => {
+        it('should remove auth token on logout', (done) => {
+
+            request(app)
+                .delete(`/users/me/token`)
+                .set('x-auth',users[0].tokens[0].token)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    User.findById(users[0]._id).then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    }).catch((e) => done(e));
+                });
         });
     });
 
